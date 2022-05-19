252166b162a7432355c7dc5e7d462f50aef48cd5
Bobo <bobo.kovacevic@gmail.com>
2022-05-20 00:04:50 +0200

    IoC container configuration

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld/>
+  <InjectionTest />
 </template>
 
 <script lang="ts">
@@ -8,34 +9,18 @@ import '@polkadot/api-augment'
 import { defineComponent, provide } from 'vue'
 // import serviceContainer from './service-container'
 import HelloWorld from './components/HelloWorld.vue'
+import InjectionTest from './components/InjectionTest.vue'
 import { TokenApiKey } from './repositories/symbols'
 import { TokenApi } from './repositories/token-api'
-import IApi from './integration/IApi'
-import Api from './integration/implementation/Api'
-import IAccountRepository from './repositories/IAccountRepository'
-import AccountRepository from './repositories/implementation/AccountRepository'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    InjectionTests
   },
   setup () {
     provide(TokenApiKey, new TokenApi())
-
-    // TODO for test purposes only, remove later
-    const api: IApi = new Api('wss://rpc.shiden.astar.network')
-    const accountRepo: IAccountRepository = new AccountRepository(api)
-
-    const fetch = async (): Promise<void> => {
-      const account = await accountRepo.getAccount('XLoLJBQoMPHMLXYhdFobSpH5GujRoUH8d1sUtaEtoBG7zaS')
-      console.log(account.toHuman())
-
-      const account1 = await accountRepo.getAccount('XLoLJBQoMPHMLXYhdFobSpH5GujRoUH8d1sUtaEtoBG7zaS')
-      console.log(account1.toHuman())
-    }
-
-    fetch()
   }
 })
 </script>
---
+++ src/components/InjectionTest.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>a</div>
+</template>
+
+<script lang="ts">
+import Component from 'vue-property-decorator'
+
+import { inject } from 'inversify-props'
+import IAccountRepository from '../repositories/IAccountRepository'
+
+@Component
+export default class InjectionTest extends Vue {
+  @inject() private accountRepo!: IAccountRepository
+
+  protected async mounted (): Promise<void> {
+    const res = await this.accountRepo.getAccount('XLoLJBQoMPHMLXYhdFobSpH5GujRoUH8d1sUtaEtoBG7zaS')
+
+    console.log(res.toHuman())
+  }
+}
+</script>
